43ca2d178fc5aaf014c21fdac8ab594a9b92fce4
Pedro de Brito <pedro.debrito@wonderkind.com>
2018-11-19 11:24:58 +0100

    Add benchmark

--- balancer_test.go
+++ balancer_test.go
@@ -26,3 +26,26 @@ func TestBalancer_Balance(t *testing.T) {
 		t.Errorf("got %v want %v", got, want)
 	}
 }
+
+func BenchmarkBalancer_Balance(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		holdings := map[Asset]Holding{
+			"eth": {20, 200},
+			"btc": {0.5, 5000},
+		}
+		index := map[Asset]float64{
+			"eth": 0.3,
+			"btc": 0.7,
+		}
+
+		got := Balance(holdings, index)
+		want := map[Asset]Trade{
+			"eth": {"sell", "10.25"},
+			"btc": {"buy", "0.41"},
+		}
+
+		if !reflect.DeepEqual(got, want) {
+			b.Errorf("got %v want %v", got, want)
+		}
+	}
+}
