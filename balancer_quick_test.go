54dd3a70781dec24d08eadcd22019e74fcac9888
Pedro de Brito <pedro.debrito@wonderkind.com>
2019-01-12 15:04:08 +0100

    test: ensure resulting index is equal to requested index

--- balancer_quick_test.go
+++ balancer_quick_test.go
@@ -18,7 +18,7 @@ type fakeAccount struct {
 }
 
 func (f fakeAccount) Generate(rand *rand.Rand, size int) reflect.Value {
-	holdings := generateHoldingsNumbering(2)
+	holdings := generateHoldingsNumbering(rand.Intn(99) + 1)
 
 	return reflect.ValueOf(fakeAccount{
 		Holdings: holdings,
@@ -26,36 +26,50 @@ func (f fakeAccount) Generate(rand *rand.Rand, size int) reflect.Value {
 	})
 }
 
-func TestBalancer_ResultValueEqualToInput(t *testing.T) {
+func TestBalancer_ResultingIndexEqualToInputIndex(t *testing.T) {
 	assertion := func(f fakeAccount) bool {
-		valueBefore := value(f.Holdings)
-
 		trades := Balance(f.Holdings, f.Index)
 
 		holdingsAfter := execute(trades, f.Holdings)
-		valueAfter := value(holdingsAfter)
 
-		sub := valueAfter.Sub(valueBefore)
-		if !sub.Equal(decimal.Zero) {
-			fmt.Printf("Value before: %v \n", valueBefore)
-			fmt.Printf("Value after: %v \n", valueAfter)
-			fmt.Printf("difference: %v \n", sub)
-		}
+		indexAfter := calculateIndex(holdingsAfter)
 
-		return valueAfter.Equal(valueBefore)
+		return indexesAreEqual(f.Index, indexAfter)
 	}
+
 	if err := quick.Check(assertion, nil); err != nil {
 		if e, ok := err.(*quick.CheckError); ok {
 			for _, value := range e.In {
 				fa := value.(fakeAccount)
 				fmt.Printf("Holdings: %v\n", fa.Holdings)
-				fmt.Printf("Index: %v\n", fa.Index)
+				fmt.Printf("Desired index: %v\n", fa.Index)
 				fmt.Printf("Trades: %v\n", Balance(fa.Holdings, fa.Index))
 			}
 		}
 
 		t.Error(err)
 	}
+}
+
+func calculateIndex(holdings map[Asset]Holding) map[Asset]decimal.Decimal {
+	index := make(map[Asset]decimal.Decimal)
+	value := value(holdings)
+	for asset, holding := range holdings {
+		index[asset] = holding.Value.Mul(holding.Quantity).Div(value)
+	}
+	return index
+}
+
+func indexesAreEqual(i1, i2 map[Asset]decimal.Decimal) bool {
+	if len(i1) != len(i2) {
+		return false
+	}
+	for asset, amount := range i1 {
+		if amount2, ok := i2[asset]; !ok || !amount.Equal(amount2) {
+			return false
+		}
+	}
+	return true
 }
 
 func generateHoldingsNumbering(n int) map[Asset]Holding {
